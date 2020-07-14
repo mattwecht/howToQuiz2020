50ce45856a48239c9287a57ea15305ad8b421c29
Matt Wecht <mattwecht@gmail.com>
2020-07-14 10:28:39 -0400

    Initial Commit

---
+++ howToQuiz2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  howToQuiz2020
+//
+//  Created by Matthew Wecht on 7/14/20.
+//  Copyright © 2020 Matthew Wecht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
